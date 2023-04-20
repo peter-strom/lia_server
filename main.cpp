a1f42017fcaf078e148b65f1d9420cffd8ada228
peter <peter@kungariket.se>
2023-04-20 13:12:51 +0200

    added SO_REUSADDR to remove binding problem (98)

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <fstream>
-using namespace std;
+
 //Server side
 int main(int argc, char *argv[])
 {
     //for the server, we only need to specify a port number
     if(argc != 2)
     {
-        cerr << "Usage: port" << endl;
+        std::cerr << "Usage: port" << std::endl;
         exit(0);
     }
     //grab the port number
@@ -41,18 +41,24 @@ int main(int argc, char *argv[])
     int serverSd = socket(AF_INET, SOCK_STREAM, 0);
     if(serverSd < 0)
     {
-        cerr << "Error establishing the server socket" << endl;
+        std::cerr << "Error establishing the server socket" << std::endl;
         exit(0);
     }
+    int enable = 1;
+    if(setsockopt(serverSd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0)
+    {
+        std::cerr << "Error setsockopt(SO_REUSADDR)" << std::endl;
+    }
+
     //bind the socket to its local address
     int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, 
         sizeof(servAddr));
     if(bindStatus < 0)
     {
-        cerr << "Error binding socket to local address" << endl;
+        std::cerr << "Error binding socket to local address :" << errno << std::endl;
         exit(0);
     }
-    cout << "Waiting for a client to connect..." << endl;
+    std::cout << "Waiting for a client to connect..." << std::endl;
     //listen for up to 5 requests at a time
     listen(serverSd, 5);
     //receive a request from client using accept
@@ -64,10 +70,10 @@ int main(int argc, char *argv[])
     int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
     if(newSd < 0)
     {
-        cerr << "Error accepting request from client!" << endl;
+        std::cerr << "Error accepting request from client!" << std::endl;
         exit(1);
     }
-    cout << "Connected with client!" << endl;
+    std::cout << "Connected with client!" << std::endl;
     //lets keep track of the session time
     struct timeval start1, end1;
     gettimeofday(&start1, NULL);
@@ -76,18 +82,18 @@ int main(int argc, char *argv[])
     while(1)
     {
         //receive a message from the client (listen)
-        cout << "Awaiting client response..." << endl;
+        std::cout << "Awaiting client response..." << std::endl;
         memset(&msg, 0, sizeof(msg));//clear the buffer
         bytesRead += recv(newSd, (char*)&msg, sizeof(msg), 0);
         if(!strcmp(msg, "exit"))
         {
-            cout << "Client has quit the session" << endl;
+            std::cout << "Client has quit the session" << std::endl;
             break;
         }
-        cout << "Client: " << msg << endl;
-        cout << ">";
-        string data;
-        getline(cin, data);
+        std::cout << "Client: " << msg << std::endl;
+        std::cout << ">";
+        std::string data;
+        getline(std::cin, data);
         memset(&msg, 0, sizeof(msg)); //clear the buffer
         strcpy(msg, data.c_str());
         if(data == "exit")
@@ -103,10 +109,10 @@ int main(int argc, char *argv[])
     gettimeofday(&end1, NULL);
     close(newSd);
     close(serverSd);
-    cout << "********Session********" << endl;
-    cout << "Bytes written: " << bytesWritten << " Bytes read: " << bytesRead << endl;
-    cout << "Elapsed time: " << (end1.tv_sec - start1.tv_sec) 
-        << " secs" << endl;
-    cout << "Connection closed..." << endl;
+    std::cout << "********Session********" << std::endl;
+    std::cout << "Bytes written: " << bytesWritten << " Bytes read: " << bytesRead << std::endl;
+    std::cout << "Elapsed time: " << (end1.tv_sec - start1.tv_sec) 
+        << " secs" << std::endl;
+    std::cout << "Connection closed..." << std::endl;
     return 0;   
 }
